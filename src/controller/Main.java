712d53009c0a96be137ba50cd699972d7566f10a
sidgopinath <sdg23@duke.edu>
2015-01-27 15:59:40 -0500

    Basic skeleton methods for all controller classes implemented.

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,5 +1,30 @@
 package controller;
 
-public class Main {
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+	private CellSocietyController myCellSocietyController;
+	public static final int HEIGHT = 600;
+	public static final int WIDTH = 600;
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Cell Society");
+		myCellSocietyController = new CellSocietyController(WIDTH, HEIGHT);
+		stage.setScene(myCellSocietyController.getScene());
+		stage.show();
+		KeyFrame frame = myCellSocietyController.getKeyFrame(60);
+		Timeline animationTimeline = new Timeline();
+		animationTimeline.setCycleCount(Timeline.INDEFINITE);
+		animationTimeline.getKeyFrames().add(frame);
+		animationTimeline.play();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
