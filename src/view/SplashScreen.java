59dc8facebf50d53e1c6e9084a306bad68ca2d66
Sunjeev <svd12@duke.edu>
2015-01-27 14:26:39 -0500

    Fixed SplashScreen Skeleton and Started Simulation Super Class

--- src/view/SplashScreen.java
+++ src/view/SplashScreen.java
@@ -6,6 +6,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 
 public class SplashScreen {
+	private Group myRoot;
 	
 	/**
 	 * This function will initialize the splash screen. The splash screen needs to display text that says the title
@@ -14,8 +15,9 @@ public class SplashScreen {
 	 * This function will also have to have add button that takes it to the FileLoaderScreen class.
 	 */
 	public Scene init(Group root, int width, int height){
-		//addTitle(root)
-		//addLoadButton(root)
+		//myRoot = root
+		//addTitle()
+		//addLoadButton()
 		return new Scene(root, width, height, Color.WHITE);
 	}
 	
@@ -23,7 +25,7 @@ public class SplashScreen {
 	 * This function will add the title. This is example code of what it might do. It will have to get the name of the
 	 * program, and position of the title from the .properties file which will then dictate where it goes.
 	 */
-	private void addTitle(Group root){
+	private void addTitle(){
 		//String splashTitle = getProp().getProperty("title");
 		//int xTitle = getProp().getProperty("TITLE_X");
 		//int yTitle = getProp().getProperty("TITLE_Y");
@@ -33,12 +35,12 @@ public class SplashScreen {
 	 * This function will add the button to the scene. This includes all the formatting of the button.
 	 * It will have to get the button's position from the .properties file. 
 	 */
-	private void addLoadButton(Group root){
+	private void addLoadButton(){
 		//button.setOnAction(e -> goToFileLoaderScreen);
 	}
 	
 	/**
-	 * This function, when clicked, will take the user to the fileLoaderScreen class, which will allow the user to
+	 * This function, when clicked, will take the user to the fileLoader class, which will allow the user to
 	 * select a file they want to load.
 	 */
 	private void goToFileLoaderScreen(KeyEvent e){
