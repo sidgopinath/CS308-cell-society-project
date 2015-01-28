ca0f31bb15a381c7c933a6e6b59914e998f6490e
Sunjeev <svd12@duke.edu>
2015-01-27 19:10:14 -0500

    Started Simulation Class

--- src/model/Simulation.java
+++ src/model/Simulation.java
@@ -1,5 +1,23 @@
 package model;
 
-public abstract class Simulation {
+import java.util.HashMap;
 
+import javafx.scene.Scene;
+
+public abstract class Simulation {
+	
+	/**
+	 * starts initSimScreen in Simulation Screen, then calls the methods to fill grid
+	 * @return
+	 */
+	public Scene initSim(){
+		return null;
+	}
+	
+	/**
+	 * fill grid with squares that have the right values given the parameters 
+	 */
+	public void fillGrid(HashMap<String, Integer> param){
+		
+	}
 }
