8834d6e9d2176438f8db6f9b65f925ec1c43bf0a
sidgopinath <sdg23@duke.edu>
2015-02-01 10:00:42 -0500

    Heavy refactoring of XMLParser. Added comments and methods in Controller. Minor changes to XML Format.

--- src/controller/CellSocietyController.java
+++ src/controller/CellSocietyController.java
@@ -1,8 +1,9 @@
 package controller;
 
+import java.io.File;
+import java.util.HashMap;
 import java.util.Random;
 
-import view.SplashScreen;
 import javafx.animation.KeyFrame;
 import javafx.scene.Group;
 import javafx.scene.Node;
@@ -11,6 +12,9 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
 import model.Simulation;
+import model.SimulationFire;
+import view.FileLoaderScreen;
+import view.SplashScreen;
 
 public class CellSocietyController {
 
@@ -18,9 +22,25 @@ public class CellSocietyController {
 	private Scene myScene;
 	private Group myGroup;
 	public static final Random RANDOM_NUM_GEN = new Random();
+	private HashMap<String, String> myParameters = new HashMap<String, String>();
+	private Integer myGrid[][];
+	private int splashWidth;
+	private int splashHeight;
 	
-	public CellSocietyController(int width, int height){
-		//initialize splash screen and set up scene
+	public CellSocietyController(int width, int height) {
+		// initialize splash screen and set up scene
+		splashWidth = width;
+		splashHeight = height;
+		displaySplashScreen(splashWidth, splashHeight);
+		myScene = new Scene((Parent) myGroup, width, height, Color.WHITE);
+	}
+
+	
+	/**
+	 * Extracted method that displays splash screen
+	 * Called from constructor to set up first screen
+	 */
+	private void displaySplashScreen(int width, int height) {
 		SplashScreen splash = new SplashScreen();
 		splash.setLoadButtonAction(e -> transitionToFileLoaderScreen());
 		Node splashNode = splash.getNode();
@@ -30,53 +50,123 @@ public class CellSocietyController {
 				- splashNode.getBoundsInLocal().getWidth() / 2);
 		splashNode.setTranslateY(height / 2
 				- splashNode.getBoundsInLocal().getHeight() / 2);
-		myScene = new Scene((Parent) myGroup, width, height, Color.WHITE);
 	}
-	
-	public Scene getScene(){
+
+	public Scene getScene() {
 		return myScene;
 	}
-	
-	public KeyFrame getKeyFrame(int frameRate){
-		return new KeyFrame(Duration.millis(1000/frameRate), e -> update());
+
+	public KeyFrame getKeyFrame(int frameRate) {
+		return new KeyFrame(Duration.millis(1000 / frameRate), e -> update());
 	}
 
+	
+	/**
+	 * Update method called every frame
+	 * Calls simulation's update method
+	 * Checks for a stopped simulation, transitions, and the program end
+	 */
 	private void update() {
-		//update program, check for transition conditions at a high level
-		myCurrentSimulation.updateGrid();
+		if (myCurrentSimulation != null) {
+			myCurrentSimulation.updateGrid();
+			checkSimulationStop();
+		}
 		checkForTransition();
-		checkSimulationStop();
 		checkProgramEnd();
-		
+
 	}
+
 	
-	private void checkProgramEnd() {
-		//check if program should stop. if so, call end program
-	}
-
+	/**
+	 * Not certain on this method yet
+	 * Thought it could check for a transition between screens
+	 */
 	private void checkForTransition() {
-		//check for transition to a new simulation or different screen?
-		
+
 	}
 
-	private void checkSimulationStop(){
-		//checks if the current simulation has stopped or been stopped by user
-		//does this by calling a method in Simulation that will return boolean
-		if(myCurrentSimulation.isStopped()){
-			//do stuff
+	/**
+	 * Checks if the current simluation has stopped or ben stopped
+	 * Calls a method in Simulation that will return a boolean
+	 * If it has been set to stop, controller will stop simulation
+	 */
+	private void checkSimulationStop() {
+		if (myCurrentSimulation.isStopped()) {
+			myCurrentSimulation.stopSim();
+		}
+	}
+
+	
+	/**
+	 * Called when an XML file needs to be read. Reads an XML file using the
+	 * parser and stores the Int grid and Parameters
+	 */
+	private void readXML(File XMLFile) {
+		XMLParser newParser = new XMLParser(XMLFile);
+		myGrid = newParser.getGrid();
+		myParameters = newParser.getParameters();
+		transitionToSimulation();
+	}
+
+	
+	/**
+	 * Method called when the load button has been pressed Opens up the file
+	 * loader screen by calling the view class There may be an easier way to
+	 * load files. This is a framework that can be changed. This method gets a
+	 * File that will then be passed into XMLParser.
+	 */
+	private void transitionToFileLoaderScreen() {
+		FileLoaderScreen fileLoaderScreen = new FileLoaderScreen();
+		myGroup.getChildren().clear();
+		Node node = fileLoaderScreen.getNode();
+		myGroup.getChildren().add(fileLoaderScreen.getNode());
+		node.setTranslateX(Main.WIDTH / 2 - node.getBoundsInLocal().getWidth()
+				/ 2);
+		node.setTranslateY(Main.HEIGHT / 2
+				- node.getBoundsInLocal().getHeight() / 2);
+		File inputFile = fileLoaderScreen.getFile(); // not sure if we should
+														// implement this in the
+														// view?
+		readXML(inputFile);
+	}
+
+	
+	/**
+	 * Called after the XML file has been made. Transitions to new simulation
+	 * Potential for many bad if statements here. How to avoid? 
+	 * Examples of bad code below
+	 */
+	private void transitionToSimulation() {
+		if (myParameters.get("simType") == "fire") {
+			myCurrentSimulation = new SimulationFire();
 		}
 	}
 	
-	private void transitionToFileLoaderScreen(){
-		//opens file loader screen
-		//not sure if this is a necessary method
+	/**
+	 * For transitioning back to splash screen
+	 * Called at end of program perhaps? Might not be necessary
+	 */
+	private void transitionToSplash(){
+		myGroup.getChildren().clear();
+		displaySplashScreen(splashWidth, splashHeight);
 	}
+
 	
-	private void transitionToSimulationScreen(){
-		//transitions to chosen simulation screen
+	/**
+	 * Checks if the program has ended
+	 * If it has ended, will call program end
+	 * Likely unnecessary method
+	 */
+	private void checkProgramEnd() {
+	
+		// check if program should stop. if so, call end program
 	}
+
 	
-	private void endProgram(){
+	/**
+	 * Called to end program. Likely unnecessary.
+	 */
+	private void endProgram() {
 		System.exit(0);
-	}	
+	}
 }
--- src/controller/XMLParser.java
+++ src/controller/XMLParser.java
@@ -1,74 +1,110 @@
 package controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class XMLParser {
 
 	private HashMap<String, String> myParameters = new HashMap<String, String>();
-	private Integer myGrid[][]; //needs a Square class for this to work
+	private Integer myGrid[][];
 	
 	public XMLParser(File XMLFile){
 		
 		try{
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(XMLFile);
-			doc.getDocumentElement().normalize();
-			
-			System.out.println("Root element: " + doc.getDocumentElement().getNodeName() + "\n"); //for debugging
-			
-			//READ PARAMETERS (make into function)
-			//clean this code below up somehow? seems like an unecessary number of lines
-			NodeList parameterList = doc.getDocumentElement().getElementsByTagName("parameters");
-			Node parameter = parameterList.item(0);
-			NodeList parameterChildren = parameter.getChildNodes();
-			
-			for(int i=0; i<parameterChildren.getLength(); i++){
-				Node currentParam = parameterChildren.item(i);
-				myParameters.put(currentParam.getNodeName(), currentParam.getNodeValue());
-				System.out.println("Current Param:" + currentParam.getNodeType() + "--" + currentParam.getNodeValue()); //for debugging
-			}
-			
-			//READ GRID (make into function)
-			//also clean this code up. It is practically duplicated from above too
-			NodeList gridList = doc.getDocumentElement().getElementsByTagName("grid");
-			Node grid = gridList.item(0);
-			NodeList gridChildren = grid.getChildNodes();
-			
-			for(int i=0; i<gridChildren.getLength(); i++){
-				Node currentRow = gridChildren.item(i);
-				String rowString = currentRow.getNodeValue();
-				String[] splitRow = rowString.split(" ");
-				System.out.println("Split Row " + splitRow);
-				
-				//Fill grid with new "squares"
-				for(int j=0; j<splitRow.length; j++){
-					Integer newSquare = Integer.parseInt(splitRow[j]);
-					myGrid[i][j] = newSquare;
-				}
-			}
+			Document doc = initializeDoc(XMLFile);
+			NodeList parameterChildren = initializeNodeList(doc, "parameters");		
+			readParameters(parameterChildren);
+			NodeList gridChildren = initializeNodeList(doc, "grid");		
+			readGrid(gridChildren);
 		}
 		
 		catch (Exception e){
 			e.printStackTrace();
 		}
 	}
+
+	/**
+	 * Read in the parameters
+	 * Put them in the HashMap
+	 */
+	private void readParameters(NodeList parameterChildren) {
+		for(int i=0; i<parameterChildren.getLength(); i++){
+			Node currentParam = parameterChildren.item(i);
+			myParameters.put(currentParam.getNodeName(), currentParam.getNodeValue());
+			System.out.println("Current Param:" + currentParam.getNodeType() + "--" + currentParam.getNodeValue()); //for debugging
+		}
+	}
+
+
+	/**
+	 * Read in the Grid row by row
+	 * Create the new "squares" by placing ints in the grid array
+	 */
+	private void readGrid(NodeList gridChildren) {
+		for(int i=0; i<gridChildren.getLength(); i++){
+			Node currentRow = gridChildren.item(i);
+			String rowString = currentRow.getNodeValue();
+			String[] splitRow = rowString.split(" ");
+			System.out.println("Split Row " + splitRow); //for debugging
+			
+			for(int j=0; j<splitRow.length; j++){
+				Integer newSquare = Integer.parseInt(splitRow[j]);
+				myGrid[i][j] = newSquare;
+			}
+		}
+	}
+
+
+	/**
+	 * Function to do initial set-up
+	 * Returns a node list of all elements of one type
+	 * Ex. returns list of parameters or list of grid rows
+	 */
+	private NodeList initializeNodeList(Document doc, String element) {
+		NodeList elementList = doc.getDocumentElement().getElementsByTagName(element);
+		Node elementNode = elementList.item(0);
+		NodeList elementChildren = elementNode.getChildNodes();
+		return elementChildren;
+	}
+
+
+	/**
+	 * Does initial set-up of document file.
+	 */
+	private Document initializeDoc(File XMLFile)
+			throws ParserConfigurationException, SAXException, IOException {
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(XMLFile);
+		doc.getDocumentElement().normalize();
+		System.out.println("Root element: " + doc.getDocumentElement().getNodeName() + "\n"); //for debugging
+		return doc;
+	}
 	
 	
-	//function to return Array that signifies the Grid
+	/**
+	 * Returns myGrid
+	 * Called by controller
+	 */
 	public Integer[][] getGrid(){
 		return myGrid;
 	}
 	
-	//function to return HashMap of parameters
+	
+	/**
+	 * Returns HashMap of parameters
+	 * Called by controller
+	 */
 	public HashMap<String, String> getParameters(){
 		return myParameters;
 	}
