5c42e9d8b21377bb6b975a9499deeb9b5ba69c91
Janan Zhu <janan.zhu@duke.edu>
2015-02-02 01:19:59 -0500

    completed draft of simulation predator

--- src/model/Simulation.java
+++ src/model/Simulation.java
@@ -1,44 +1,48 @@
 package model;
 
 import java.util.Map;
-
+import view.SimulationScreen;
 import javafx.scene.Scene;
 
 public abstract class Simulation {
-		private Square myGrid [][];
-		
-		/**
-		 * first fill grid with appropriate square types
-		 * then pass squares their appropriate neighbors
-		 * Then update squares
-		 */
-		public void runSim(Map<String, String> paramMap){
-			myGrid = fillGrid(paramMap);
-			updateGrid(myGrid);
-			
-		}
-		
-		/**
-		 * fill grid with squares that have the right values given the parameters and the type of each space
-		 * initialize grid, and fill
-		 * @param paramMap 
-		 */
-		abstract Square[][] fillGrid(Map<String, String> paramMap);
-		
-		/**
-		 * first it will pass each square its neighbors
-		 *  then it will go through and call each cell to update.
-		 *  update view
-		 */
-		abstract void updateGrid();
-		
-		/**
-		 * pass each square its neighbors
-		 */
-		abstract void updateNeighbors();
-		
-	}
+    protected SimulationScreen myView;
+    
+    public Simulation(){
+        myView = new SimulationScreen();
+    }
+    
+    /**
+     * first fill grid with appropriate square types
+     * then pass squares their appropriate neighbors
+     * Then update squares
+     */
+    public void runSim(int[][] grid, Map<String, String> paramMap){
+        fillGrid(grid, paramMap);
+        updateGrid();
 
-	
-	
+    }
+
+    /**
+     * fill grid with squares that have the right values given the parameters and the type of each space
+     * initialize grid, and fill
+     * @param paramMap 
+     */
+    abstract void fillGrid(int[][] initialGrid,Map<String, String> paramMap);
+
+    /**
+     * first it will pass each square its neighbors
+     *  then it will go through and call each cell to update.
+     *  update view
+     */
+    abstract void updateGrid();
+
+    /**
+     * pass each square its neighbors
+     */
+    abstract void updateNeighbors();
+
+}
+
+
+
 
