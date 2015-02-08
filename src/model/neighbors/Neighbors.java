83baa8354132324bc05a5d0809735c5d5d101d4d
Janan Zhu <janan.zhu@duke.edu>
2015-02-07 21:20:14 -0500

    basic inheritance hierarchy for implementing different grids and shapes

--- src/model/neighbors/Neighbors.java
+++ src/model/neighbors/Neighbors.java
@@ -1,9 +1,18 @@
 package model.neighbors;
+import java.util.List;
+import model.cells.Cell;
+import model.gridrules.GridRules;
 
 public abstract class Neighbors {
 
-	//will contain methods to construct neighbor list and check neighbors, etc.
-	//extended by different type of neighbor functions?
-	//or perhaps different neighbor functions will all be in this class?
-	
+    //will contain methods to construct neighbor list and check neighbors, etc.
+    //extended by different type of neighbor functions?
+    //or perhaps different neighbor functions will all be in this class?
+    public abstract List<Cell> getCardinalNeighbors(Cell[][] grid, int x,
+                                                    int y, GridRules rules);
+    public abstract List<Cell> getDiagonalNeighbors(Cell[][] grid, int x, int y,
+                                                    GridRules rules);
+    public abstract List<Cell> getAllNeighbors(Cell[][] grid, int x, int y,
+                                               GridRules rules);
+
 }
